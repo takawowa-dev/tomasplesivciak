86d9ef9575d6d49553ac177d4c70162e54d7335a
takawowa-dev <takawowa@gmail.com>
2026-07-03 11:27:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const slideshows = document.querySelectorAll(".slideshow, .slideshow-1280x1000, .slideshow-1280x1080");
-slideshows.forEach(slideshow => {
-    const imgs = slideshow.querySelectorAll("img");
-    let index = 0;
+document.addEventListener("DOMContentLoaded", () => {
 
-    // první obrázek viditelný
-    imgs[0].style.opacity = 1;
+    const slideshows = document.querySelectorAll(".slideshow, .slideshow-1280x1000, .slideshow-1280x1080");
 
-    setInterval(() => {
-        imgs[index].style.opacity = 0;
+    slideshows.forEach(slideshow => {
+        const imgs = slideshow.querySelectorAll("img");
 
-        index = (index + 1) % imgs.length;
+        if (imgs.length === 0) return;
 
-        imgs[index].style.opacity = 1;
-    }, 1000);
+        let index = 0;
+
+        // schovat všechny
+        imgs.forEach(img => img.style.opacity = 0);
+
+        // první viditelný
+        imgs[0].style.opacity = 1;
+
+        setInterval(() => {
+            imgs[index].style.opacity = 0;
+
+            index = (index + 1) % imgs.length;
+
+            imgs[index].style.opacity = 1;
+        }, 1000);
+    });
+
 });
